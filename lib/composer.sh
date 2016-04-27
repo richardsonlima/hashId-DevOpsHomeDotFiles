1da7d0275f7f7a2bdfdf1fb54b46c9c8666a1723
Richardson Lima <contatorichardsonlima@gmail.com>
2016-04-27 10:56:09 -0300

    commit all files

---
+++ lib/composer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PACKAGE='curl'
+
+sudo apt-get install $PACKAGE
+
+# Install composer.phar
+echo -e '\033[1;32m [+]Install to composer.phar \033[0m'
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin/
+
+# Rename composer.phar composer
+echo '\033[1;32m Rename composer.phar composer \033[0m'
+sudo mv /usr/local/bin/composer.phar /usr/local/bin/composer
+
