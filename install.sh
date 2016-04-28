6a3189773289fd6c179a5afec8fbe69140152e22
Richardson Lima <contatorichardsonlima@gmail.com>
2016-04-27 23:17:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ copyright="Copyright 2016 - ${author}, ${website}"
 #
 
 disclaimer(){
-echo -e ""
+echo 
             echo -e "      ==============================================================================="
             echo -e "        ${NOTICE} ${PROGRAM_name} ${NORMAL}"
             echo -e "      ==============================================================================="
@@ -54,7 +54,7 @@ echo -e ""
             echo -e "        *** ATTENTION: This program was tested on Ubuntu 16.04 desktop "
             echo -e "        $copyright "
             echo -e "      ==============================================================================="
-            echo -e ""
+            echo 
             sleep 5
 ##
     echo
@@ -64,17 +64,16 @@ echo -e ""
     echo -e "\033[0;36m [+] Distro: \033[m" $(python -c 'import platform; print platform.dist()' )
     echo -e "\033[0;36m [+] Version/Kernel: \033[m" $(python -c 'import platform; print platform.release()' )
     echo -e "\033[0;36m [+] Arch: \033[m" $(python -c 'import platform; print platform.machine()' )
-    #echo -e "\033[1;32m [+] Server ID: \033[m"; sudo dmidecode -s system-uuid
     echo -e "\033[0;36m [+] UUID: \033[m" $(python -c 'import os; os.system("sudo dmidecode -s system-uuid")')
     echo
+    sleep 5
 ##
 }
 
 CURRENT=`pwd`
-INSTALL_COMPOSER=true
 INSTALL_POWERLINE=true
 INSTALL_TERM=true
-#INSTALL_ANSIBLE=true
+INSTALL_ATOM=true
 INSTALL_ANSIBLE=false
 
 # Parsing options
@@ -82,9 +81,9 @@ if [ $# -gt 0 ] ; then
     for arg in $@ ; do
         if [ $arg = '--no-powerline' ] ; then
             INSTALL_POWERLINE=false
-        elif [ $arg = '--no-composer' ] ; then
-            INSTALL_COMPOSER=false
         elif [ $arg = '--no-term' ] ; then
+            INSTALL_TERM=false
+        elif [ $arg = '--no-atom' ] ; then
             INSTALL_TERM=false
         elif [ $arg = '--ansible' ] ; then
             INSTALL_ANSIBLE=true
@@ -95,8 +94,8 @@ if [ $# -gt 0 ] ; then
             printf "  ./install.sh [options]\n\n"
             printf "\033[0;33mOptions:\033[0m\n"
             printf "  \033[0;32m--no-powerline\033[0m\t\tDon't install powerline\n"
-            printf "  \033[0;32m--no-composer\033[0m\t\tDon't install composer\n"
             printf "  \033[0;32m--no-term\033[0m\t\tDon't install term terminator\n"
+            printf "  \033[0;32m--no-atom\033[0m\t\tDon't install atom - The hackable text editor\n"
             printf "  \033[0;32m--ansible\033[0m\t\tInstall ansible and a configuration\n"
             printf "  \033[0;32m--help\033[0m\t\tDisplay this help message\n"
             exit 0;
@@ -104,7 +103,7 @@ if [ $# -gt 0 ] ; then
     done
 fi
 
-PACKAGE='poweline git jq git-core tig curl php python-pip python-psutil htop glances rdesktop cifs-utils vim tmux gtkterm openssh-server filezilla'
+PACKAGE='build-essential poweline git jq git-core tig curl php python-pip python-psutil htop glances rdesktop cifs-utils vim tmux gtkterm openssh-server filezilla'
 
 if $INSTALL_TERM ; then
    disclaimer
@@ -112,6 +111,8 @@ if $INSTALL_TERM ; then
         cat ~/.config/terminator/config > ~/.config/terminator/config.backup
         rm -rf ~/.config/terminator
         printf "\033[0;36m [+] Existing .config terminator > save\033[0m \033[0;32m~/.config/terminator/config.backup\033[0m\n"
+        echo
+        sleep 3
     fi
 
     ln -sf $CURRENT/_config/terminator/ ~/.config/terminator
@@ -125,6 +126,8 @@ if $INSTALL_ANSIBLE ; then
         cat ~/.ansible.cfg > ~/ansible.cfg.backup
         rm -f ~/.ansible.cfg
         printf "\033[0;36m [+] Existing .ansible.cfg > save\033[0m \033[0;32m~/ansible.cfg.backup\033[0m\n"
+        echo
+        sleep 3
     fi
 
     #sudo add-apt-repository ppa:rquillo/ansible
@@ -134,78 +137,140 @@ if $INSTALL_ANSIBLE ; then
     PACKAGE="$PACKAGE ansible"
 fi
 
-printf "\033[0;32m [+] Install packages $PACKAGE\033[0m\n"
+printf "\033[0;32m [+] Installing packages $PACKAGE\033[0m\n"
 sudo apt-get update
 sudo apt-get install $PACKAGE -y
-echo ""
+echo 
+sleep 3
+echo 
 printf "\033[0;32mTest files exist ~/.gitconfig ~/.gitignore_global ~/.config/fontconfig\033[0m\n"
 if [ -f ~/.gitconfig ] ; then
     cat ~/.gitconfig > ~/gitconfig.backup
     rm -f ~/.gitconfig
+    sleep 3
+    echo 
     printf "\033[0;36m [+] Existing .gitconfig\t\t> save\033[0m \033[0;32mgitconfig.backup\033[0m\n"
+    sleep 3
 fi
 
 if [ -f ~/.gitignore_global ] ; then
     cat ~/.gitignore_global > ~/gitignore_global.backup
     rm -f ~/.gitignore_global
     printf "   \033[0;36m [+] Existing .gitignore_global\t> save\033[0m \033[0;32mgitignore_global.backup\033[0m\n"
+    echo 
+    sleep 3
 fi
 
 if [ -f ~/.config/fontconfig ] ; then
     rm -rf ~/.config/fontconfig
     printf "   \033[0;36m [+] Existing .config/fontconfig\033[0m \033[0;33m=> remove\033[0m\n"
+    echo 
+    sleep 3
 fi
 
-echo ""
+echo
 printf "   \033[0;36m [+] Create symlinks: \033[0m\n"
 printf "   \033[0;36m [+] .gitconfig\033[0m\n"
 ln -sf $CURRENT/gitconfig ~/.gitconfig
+echo
+sleep 3
 printf "   \033[0;36m [+] .gitignore_global\033[0m\n"
 ln -sf $CURRENT/gitignore_global ~/.gitignore_global
+echo
+sleep 3
 printf "   \033[0;36m [+] .config/fontconfig\033[0m\n"
 ln -sf $CURRENT/_config/fontconfig/ ~/.config/fontconfig
+echo
+sleep 3
 printf "   \033[0;36m [+] .fonts\033[0m\n"
 ln -sf $CURRENT/_fonts/ ~/.fonts
+echo
+sleep 3
 
-echo ""
-printf "\033[0;36m [+] Load fonts: \033[0m\n"
+printf "   \033[0;36m [+] Loading fonts: \033[0m\n"
 fc-cache -vf ~/.fonts
-echo ""
-
-if $INSTALL_COMPOSER ; then
-    echo -e '\033[0;36m [+] Installation de composer  \033[0m'
-    chmod +x $CURRENT/lib/composer.sh && sudo $CURRENT/lib/composer.sh
-fi
+echo 
+sleep 3
 
 if $INSTALL_POWERLINE ; then
-    echo -e '\033[0;36m [+] Powerline Installation \033[0m'
-    echo ""
-    function add_line() {
-    LINE="$1"
-    FILE=$2
-    grep -F -q "${LINE}" ${FILE} || (echo -e '\n# Powerline shell changes' >> ${FILE} && echo ${LINE} >> ${FILE})
-}
+    echo 
+    echo -e '\033[0;36m [+] Setting Powerline for Bash Shell and Vim Statuslines \033[0m'
+    echo
+    echo -e '\033[0;36m [+] Installing Powerline on Bash Shell \033[0m'
+    echo 
+    sudo pip install setuptools
+    sudo pip install git+git://github.com/Lokaltog/powerline
+    
+    echo -e '\033[0;36m [+] Installing Powerline Fonts in Linux \033[0m'
+    cp $CURRENT/powerline-config/PowerlineSymbols.otf /usr/share/fonts/
+    echo
+    echo -e '\033[0;36m [+] Updating your system’s font cache \033[0m'
+    sudo fc-cache -vf /usr/share/fonts/
+    echo -e '\033[0;36m [+] Installing the fontconfig file \033[0m'
+    cp $CURRENT/powerline-config/10-powerline-symbols.conf /etc/fonts/conf.d/
+    echo
+    echo -e '\033[0;36m [+] Making your terminal to support 256color \033[0m' 
+    echo "export TERM="screen-256color" " >> ~/.bashrc
+    echo -e '\033[0;36m [+] Enable Powerline on Bash Shell \033[0m' 
+    echo "powerline-daemon -q" >> ~/.bashrc
+    echo "POWERLINE_BASH_CONTINUATION=1" >> ~/.bashrc
+    echo "POWERLINE_BASH_SELECT=1" >> ~/.bashrc
+    echo ". /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh" >> ~/.bashrc 
+    echo -e '\033[0;36m [+] Getting the location of installed powerline \033[0m'
+    sudo pip show powerline-status
+    
+    powerline-daemon -q
+    POWERLINE_BASH_CONTINUATION=1
+    POWERLINE_BASH_SELECT=1
+    . /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh 
+    .  ~/.bashrc
+    echo
+    sleep 3
+    echo 
+    echo -e '\033[0;36m [+] Enabling Powerline for Vim \033[0m'
+    echo "set  rtp+=/usr/local/lib/python2.7/dist-packages/powerline/bindings/vim/" >> ~/.vimrc
+    echo "set laststatus=2" >> ~/.vimrc
+    echo "set t_Co=256" >> ~/.vimrc
+    .  ~/.vimrc
+    echo
+    sleep 3 
 
-sudo pip install setuptools
-sudo pip install powerline-status
-sudo pip install https://github.com/powerline/powerline
-#sudo pip install --user git+git://github.com/powerline/powerline
+fi
 
-LINE='if [ -d "$HOME/.local/bin" ] ; then PATH="$HOME/.local/bin:$PATH"; fi'
-add_line "${LINE}" ~/.profile
+if $INSTALL_ATOM ; then
+    echo
+    echo -e '\033[0;36m [+] Installing Atom - The hackable text editor\033[0m'
+    echo 
+    sleep 3
+    echo -e '\033[0;36m [+] Installing Nodejs \033[0m'
+    sudo apt-get install libgnome-keyring-dev fakeroot -y
+    curl -sL https://deb.nodesource.com/setup | sudo bash -
+    sudo apt-get install -y nodejs
+    echo
+    sleep 3 
 
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
-fc-cache -vf ~/.fonts
-mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+    echo -e '\033[0;36m [+] Cloning the Atom repository \033[0m'
+    git clone https://github.com/atom/atom
+    cd atom
+    echo
+    sleep 3 
 
-LINE='if [ -f ~/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh ]; then source ~/.local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh; fi'
-add_line "${LINE}" ~/.bashrc
+    echo -e '\033[0;36m [+] Checkout the latest Atom release \033[0m'
+    git fetch
+    git checkout $(git describe --tags `git rev-list --tags --max-count=1`)
+    echo
+    sleep 3 
 
-# Use the "default_leftonly" theme
-CFG_FILE=~/.local/lib/python2.7/site-packages/powerline/config_files/config.json
-jq '(.ext.shell.theme = "default_leftonly")' "${CFG_FILE}" > tmp.$$.json && mv tmp.$$.json ${CFG_FILE}
-sudo pip show powerline-status
+    echo -e '\033[0;36m [+] Building Atom \033[0m'
+    script/build
+    echo
+    sleep 3 
+
+    echo -e '\033[0;36m [+] Installing the atom and apm commands to /usr/local/bin by executing \033[0m'
+    sudo script/grunt install
+    echo
+    sleep 3 
+
 fi
 
 echo -e '\033[0;36m [+] End [!!!] \033[0m'
